c22c1a14bb08537456f00c3aa6f62d00dc844e11
Utkarsh-sin <sinhautkarsh38@gmail.com>
2023-01-16 03:16:44 +0530

    Ecapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,20 +2,22 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B b=new B();
+        B b = new B();
         b.meth();
         b.meth();
     }
-}
 
-class A{
-    public String meth() {
-        return "Invoking method from class A";
+
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
     }
-}
-class B extends A{
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
+
+    public static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
     }
 }
